5dbbde20a7126f4ffdd39924d2b1ab8f8fe0e1aa
Torkey <talgamedi@yahoo.com>
2019-12-13 11:31:03 +0300

    Last Update

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+const postCSSPlugins = [
+  require('postcss-simple-vars'), // name of the plugin that installed
+  require('postcss-nested'), // name of the plugin that installed
+  require('autoprefixer') // name of the plugin that installed
+];
+
+module.exports = {
+  entry: './app/assets/scripts/App.js',
+
+  output: {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app')
+  },
+
+  mode: 'development',
+  watch: true,
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader?url=false',
+          {
+            loader: 'postcss-loader',
+            options: { plugins: postCSSPlugins }
+          }
+        ]
+      }
+    ]
+  }
+};
