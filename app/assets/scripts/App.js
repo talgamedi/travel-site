28410245e29be0e578c5264681fd0970d1dee088
Torkey <talgamedi@yahoo.com>
2020-01-09 19:35:11 +0300

    First Step for Menu with JS

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,10 @@
 import '../styles/style.css';
+import MobileMenu from './modules/MobileMenu';
 
 if (module.hot) {
   module.hot.accept();
 }
+
+//This Code -------------------
+
+let mobileMenu = new MobileMenu();
